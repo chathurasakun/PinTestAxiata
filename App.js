ff98116fe2bddd090d6c7e599b87a1759aaf71f9
CHATHURA SAKUN ELLAWALA <sakun@villvay.com>
2019-07-04 13:25:14 +0530

    remove unnecessary props

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import ResetPin from './app/screens/ResetPin';
 class App extends PureComponent {
   render = () => {
     return (
-      <Router navigationBarStyle={{ backgroundColor: '#7573E1' }}>
+      <Router>
         <Scene key='root' hideNavBar>
           <Scene key='enterPin' component={EnterResetPin} type='replace' initial />
           <Scene key='resetPin' component={ResetPin} type='replace' />
